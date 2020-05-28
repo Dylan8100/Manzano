9127c6f5e71274b2dec14ac54a77b8e18361b168
Dylan8100 <65318303+Dylan8100@users.noreply.github.com>
2020-05-27 19:57:42 -0500

    Initial commit

---
+++ js/camunidad.js
@@ -0,0 +1,46 @@
+var imagenes = ['img/galeria/1.jpg', 'img/galeria/2.jpg','img/galeria/3.jpg']
+var cont = 0;
+
+function carrusel(contenedor)
+{
+    contenedor.addEventListener('click', e =>{
+        let atras = contenedor.querySelector('.atras'),
+        adelante = contenedor.querySelector('.adelante'),
+        img = contenedor.querySelector('img'),
+        tgt = e.target;
+
+
+        if(tgt == atras)
+        {
+            if(cont > 0)
+            {
+                img.src = imagenes[cont - 1];
+                cont--;
+            }
+            else
+            {
+                img.src = imagenes[imagenes.length - 1];
+                cont = imagenes.length - 1;
+            }
+        }
+        else if(tgt == adelante)
+        {
+            if(cont < imagenes.length -1)
+            {
+                img.src = imagenes[cont + 1];
+                cont++;
+            }
+            else
+            {
+                img.src = imagenes[0];
+                cont = 0;
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    let contenedor = document.querySelector('.contenedorSlider');
+
+    carrusel(contenedor);
+});
